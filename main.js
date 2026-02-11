686a5d6d8f26173cbf33563a5842d0c9587c62f3
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2026-02-11 20:29:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+let isDark = localStorage.getItem('dark_mode') === 'true';
+const path = window.location.pathname.includes('/post/') ? '../../' : '';
+
+const gtasanandreas_path = window.location.pathname.includes('/gta-san-andreas');
+const nba2k11_path = window.location.pathname.includes('/nba-2k11');
+const nba2k14_path = window.location.pathname.includes('/nba-2k14');
+
+fetch(path + "games-list.html")
+    .then(res => res.text())
+    .then(html => {
+    const gamesList = document.getElementById("games-list");
+    gamesList.innerHTML = html;
+
+    const items = gamesList.querySelectorAll(".post-item");
+    items.forEach((item, i) => {
+        const originalHref = item.getAttribute("href");
+        item.setAttribute("href", path + originalHref);
+        if (i === 0) {
+            if (gtasanandreas_path)
+                item.classList.add("active");
+        }
+        else if (i === 1) {
+            if (nba2k11_path)
+                item.classList.add("active");
+        }
+        else if (i === 2) {
+            if (nba2k14_path)
+                item.classList.add("active");
+        }
+    });
+});
+
+function applyTheme() {
+    const theme = document.getElementById('theme');
+    const logo = document.getElementById('logo');
+    const label = document.getElementById("toggle-label");
+    if (isDark) {
+        theme.href = path +'static/css/dark.css';
+        logo.src = path +'static/img/logo-1.png';
+        label.textContent = "Dark Mode: On";
+    }
+    else {
+        theme.href = path +'static/css/light.css';
+        logo.src = path +'static/img/logo.png';
+        label.textContent = "Dark Mode: Off";
+    }
+}
+
+function setDarkMode() {
+    if (isDark) {
+        localStorage.setItem('dark_mode', false);
+        isDark = false;
+        applyTheme(false);
+    }
+    else {
+        localStorage.setItem('dark_mode', true);
+        isDark = true;
+        applyTheme(true);
+    }
+}
+
+applyTheme(isDark);
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.onscroll = function() {
+    const scrollBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollBtn.classList.add("show");
+    }
+    else {
+        scrollBtn.classList.remove("show");
+    }
+};
